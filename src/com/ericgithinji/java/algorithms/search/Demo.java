bf38173ccde99f3f298c1d64815a4e1f48b197a6
bomb1e <ericgithinjimaina@gmail.com>
2015-10-06 11:40:36 +0000

    Refactor and make more presentable. MergeSort not yet fixed

--- src/com/ericgithinji/java/algorithms/search/Demo.java
+++ src/com/ericgithinji/java/algorithms/search/Demo.java
@@ -16,60 +16,49 @@ import com.ericgithinji.java.algorithms.sort.QuickSort;
 public class Demo {
     private static final int ONE_MILLION = 1_000_000;
     public static void main(String[] args){
-        // Use Sort Algorithms
-//        int[] tempArray = {8,6,1,2,7,3,5,4,9};
-//        BubbleSort.sort(tempArray,false);
-//        System.out.print("{");
-//        for(int i = 0; i < tempArray.length; i++){
-//            System.out.print(tempArray[i] + ",");
-//        }
-//        System.out.println("}");
-        int[] tempArray = {8,6,1,2,7,3,5,4,2};
+        
+//         Use Sort Algorithms
+        
+//        BubbleSort
+        int[] tempArray1 = {8,6,1,2,7,3,5,4,9};
+        BubbleSort.sort(tempArray1,false);
+        System.out.println("Bubble Sorted!");
         System.out.print("{");
-        for(int i = 0; i < tempArray.length; i++){
-            System.out.print(tempArray[i] + ",");
+        for(int i = 0; i < tempArray1.length; i++){
+            System.out.print(tempArray1[i] + ",");
         }
         System.out.println("}");
+        
+//        QuickSort
+        int[] tempArray = {8,6,1,2,7,3,5,4,2};
         QuickSort.sort(tempArray);
+        System.out.println("Quickly Sorted!");
         System.out.print("{");
         for(int i = 0; i < tempArray.length; i++){
             System.out.print(tempArray[i] + ",");
         }
         System.out.println("}");
         
-//        int[] tempArray2 = {8,6,1,2,7,3,5,4,9};
+//        MergeSort
+        int[] tempArray2 = {8,6,1,2,7,3,5,4,9};
 //        MergeSort.mergeSort(tempArray2);
-//        System.out.print("{");
-//        for(int i = 0; i < tempArray2.length; i++){
-//            System.out.print(tempArray2[i] + ",");
-//        }
-//        System.out.println("}");
+        System.out.println("Merge Sorted!");
+        System.out.print("{");
+        for(int i = 0; i < tempArray2.length; i++){
+            System.out.print(tempArray2[i] + ",");
+        }
+        System.out.println("}");
         
         // Use Search Algorithms
-//        int[] integerArray = new int[1000000]; 
-//        Object[] demoArray = {1,2,34,5,63,3,"Erico","Whagwan"};
-//        for(int i = 0; i < ONE_MILLION; i++) {
-//            integerArray[i] = i + 1;
-//        }
-//        The next two functions will take a long time
-//        Use them only if you know what you're doing
-//        System.out.print("Linear times is ");
-//        
-//        for(int n = 1; n < integerArray.length; n++){
-//            System.out.println(n);
-//        }
-        
-        
-//        System.out.print("Binary times is ");
-//        
-//        for(int i = 1; i < integerArray.length; i++){
-//            System.out.println(Math.log(i)/Math.log(2));
-//        }
-        
-//        System.out.println("Linear Position is: " + LinearSearch.search(demoArray,"Eric"));
-//        System.out.println("Binary Position is: " + BinarySearch.search(integerArray,1000000)); 
-//        System.out.println("Linear Position is: " + LinearSearch.search(integerArray,1000000)); 
-//        System.out.println("Binary Recursive Position is: " + BinarySearch.recSearch(integerArray,1000000));
+        int[] integerArray = new int[1000000]; 
+        Object[] demoArray = {1,2,34,5,63,3,"Erico","Whagwan"};
+        for(int i = 0; i < ONE_MILLION; i++) {
+            integerArray[i] = i + 1;
+        }
+        System.out.println("Linear Position is: " + LinearSearch.search(demoArray,"Eric"));
+        System.out.println("Binary Position is: " + BinarySearch.search(integerArray,1000000)); 
+        System.out.println("Linear Position is: " + LinearSearch.search(integerArray,1000000)); 
+        System.out.println("Binary Recursive Position is: " + BinarySearch.recSearch(integerArray,1000000));
         
     }
 }
